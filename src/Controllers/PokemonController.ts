16f24572fdbadd4c7b77c2a7f1f461d1ad468b41
Brunner <brunnel6@troulwn0123.emea.roche.com>
2017-03-06 14:34:00 +0100

    example /pokemon request

--- src/Controllers/PokemonController.ts
+++ src/Controllers/PokemonController.ts
@@ -1,11 +1,22 @@
-import { Controller, Param, Body, Get, Post, Put, Delete } from "routing-controllers";
-
-@Controller()
+import { JsonController, Param, Body, Get, Post, Put, Delete, Req, Res } from "routing-controllers";
+import * as request from 'request';
+import { Promise } from 'es6-shim';
+@JsonController()
 export class PokemonController {
-
     @Get("/pokemon")
-    getAll() {
-        return "This action returns all users";
+    getAll( @Req() req, @Res() res) {
+        var regexp = new RegExp('^(V[0-9]+_POKEMON_?.*)', 'g');
+
+        return new Promise<any>((ok, fail) => {
+            request.get('https://raw.githubusercontent.com/BrunnerLivio/pokemongo-game-master/master/versions/latest/GAME_MASTER.json', (error, response, body) => {
+                let game_master: any = JSON.parse(body)
+                let pokemons: Object[] =
+                    game_master
+                        .itemTemplates
+                        .filter(item => regexp.test(item.templateId));
+                ok(pokemons);
+            });
+        });
     }
 
     @Get("/pokemon/:id")
