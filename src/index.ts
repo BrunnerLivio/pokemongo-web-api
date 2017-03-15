25b4f53b2c2127dd340c21ee50579b19e2d27ff8
Brunner <livio.brunner.lb1@gmail.com>
2017-03-15 09:45:37 +0100

    Add log messages

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@ import 'es6-shim';
 import 'reflect-metadata';
 import { createExpressServer } from 'routing-controllers';
 import './Controllers/PokemonController';
+import * as chalk from 'chalk';
 
 let app = createExpressServer();
+console.log(`Listening on port ${chalk.green('8080')}`);
+
 app.listen(8080);
