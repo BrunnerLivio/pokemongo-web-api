79bf64d4b45b649e3b9f8f03ef1786d5371daf19
Livio <livio.brunner.lb1@gmail.com>
2017-03-20 11:31:26 +0100

    Update fetch-data script for move data

--- bin/get-data.js
+++ bin/get-data.js
@@ -2,15 +2,36 @@ var https = require('https');
 var fs = require('fs');
 var path = require('path');
 
-var lastestPokemonPath = 'https://raw.githubusercontent.com/BrunnerLivio/pokemongo-data-normalizer/master/output/pokemon.json';
-var dir = path.join(__dirname, '../src/Data');
-var file = path.join(dir, 'Pokemon.json');
+var paths = [{
+		url: 'https://raw.githubusercontent.com/BrunnerLivio/pokemongo-data-normalizer/master/output/pokemon.json',
+		path: 'Pokemon.json'
+	},
+
+	{
+		url: 'https://raw.githubusercontent.com/BrunnerLivio/pokemongo-data-normalizer/master/output/move.json',
+		path: 'Move.json'
+	}
+];
+var dir = path.join(__dirname, '../src/shared');
 if (!fs.existsSync(dir)) {
 	fs.mkdirSync(dir);
 }
 
 
-var file = fs.createWriteStream(file);
-var request = https.get(lastestPokemonPath, function (response) {
-	response.pipe(file);
-});
+var counter = 0;
+
+function getFile() {
+	console.log(`Fetch ${paths[counter].path}`)
+	var file = path.join(dir, paths[counter].path);
+	var file = fs.createWriteStream(file);
+
+	https.get(paths[counter].url, function (response) {
+		response.pipe(file);
+		counter++;
+		if (counter < paths.length) {
+			getFile();
+		}
+	});
+}
+
+getFile();
