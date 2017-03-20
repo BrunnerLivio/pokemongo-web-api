7f9d7041ed8c26e76cf7ceec26b5d2dcaa247bb3
Livio <livio.brunner.lb1@gmail.com>
2017-03-20 14:02:21 +0100

    Add syntax highlighting in preview

--- src/client/app/app.ts
+++ src/client/app/app.ts
@@ -2,10 +2,12 @@ import * as $ from 'jquery';
 import 'materialize-css/dist/js/materialize.js';
 import 'materialize-css/dist/css/materialize.css';
 import 'normalize.css';
+import * as hljs from 'highlight.js';
+import 'highlight.js/styles/default.css';
 
 import './app.scss';
 
-$(document).ready(function () {
+$(document).ready(() => {
     const url = window.location.href;
     const arr = url.split('/');
     const apiurl = arr[0] + '//' + arr[2] + '/api/v1/';
@@ -13,5 +15,11 @@ $(document).ready(function () {
 
     $.get(apiurl + 'pokemon/BULBASAUR', (data) => {
         $('#preview-output').text(JSON.stringify(data, null, 4));
+        $('pre code').each((i, block) => {
+            console.log(block);
+            hljs.highlightBlock(block);
+        });
     });
+
+
 });
